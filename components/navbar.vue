ac0667d2651185622c62dfcb8e0cdd55ea0b56eb
Robert Adamyan <robert.adamyan1@gmail.com>
2017-06-20 04:47:39 +0400

    change navbar structure

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,14 +1,18 @@
 <template>
   <div>
-    <nav v-bind:class="active" v-on:click.prevent>
-      <img src="../static/logoLumina.png"  />
-      <a href="#" v-scroll-to="{
-        el: '.problem-wrap'
-        }" class="problem" v-on:click="makeActive('problem')">Problem</a>
-      <a href="#" class="solution" v-on:click="makeActive('solution')">Solution</a>
-      <a href="#" class="services" v-on:click="makeActive('services')">Services</a>
-      <a href="#" class="contact" v-on:click="makeActive('contact')">Contact</a>
+    <nav v-bind:class="active" v-on:click.prevent class="navbar">
+      <img src="../static/logoLumina.png" class="navbar-image--logo" />
     </nav>
+    <div class="navbar-link--container">
+      <div class="navbar-link--wrap-first-item">
+        <a href="#" v-scroll-to="{
+          el: '.problem-wrap'
+          }" class="navbar-link" v-on:click="makeActive('problem')">Problem</a>
+      </div>
+      <div class="navbar-link--wrap"><a href="#" class="navbar-link" v-on:click="makeActive('solution')">Solution</a></div>
+      <div class="navbar-link--wrap"><a href="#" class="navbar-link" v-on:click="makeActive('services')">Services</a></div>
+      <div class="navbar-link--wrap"><a href="#" class="navbar-link" v-on:click="makeActive('contact')">Contact</a></div>
+    </div>
   </div>
 </template>
 
