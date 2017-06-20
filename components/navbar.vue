b65d868f3b3a0fee01d8190e3a63eb2d06feb937
Robert Adamyan <robert.adamyan1@gmail.com>
2017-06-20 19:23:28 +0400

    navbar scroll to solution, minor fixes

--- components/navbar.vue
+++ components/navbar.vue
@@ -1,26 +1,30 @@
 <template>
-<section>
-  <div v-if="top <= 300">
-    <nav >
-      <img src="../static/logoLumina.png"  />
-      <a href="#" v-scroll-to="{
-        el: '.problem-wrap'
-        }" class="problem" v-on:click="makeActive('problem')">Problem</a>
-      <a href="#" class="solution" v-on:click="makeActive('solution')">Solution</a>
-      <a href="#" class="services" v-on:click="makeActive('services')">Services</a>
-      <a href="#" class="contact" v-on:click="makeActive('contact')">Contact</a>
-    </nav>
-  </div>
-  <div v-else>
-     <nav style="background-color: #fff">
-      <img src="../static/logoLumina.png"  />
-      <a href="#" v-scroll-to="{
-        el: '.problem-wrap'
-        }" >Problem</a>
-      <a href="#" >Solution</a>
-      <a href="#">Services</a>
-      <a href="#">Contact</a>
-    </nav>
+  <section>
+    <div v-if="top <= 300">
+      <nav >
+        <img src="../static/logoLumina.png"  />
+        <a href="#" 
+           v-scroll-to="{el: '.problem-wrap'}" 
+           class="problem" 
+           v-on:click="makeActive('problem')">Problem</a>
+        <a href="#"
+           v-scroll-to="{el: '.solution-wrap'}" 
+           class="solution" 
+           v-on:click="makeActive('solution')">Solution</a>
+        <a href="#" class="services" v-on:click="makeActive('services')">Services</a>
+        <a href="#" class="contact" v-on:click="makeActive('contact')">Contact</a>
+      </nav>
+    </div>
+    <div v-else>
+      <nav style="background-color: #fff">
+        <img src="../static/logoLumina.png"  />
+        <a href="#" 
+           v-scroll-to="{el: '.problem-wrap'}">Problem</a>
+        <a href="#" 
+           v-scroll-to="{el: '.solution-wrap'}">Solution</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </nav>
     </div>
   </section>
 </template>
