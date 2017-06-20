e60a27713a1f5c59014482f0b5277df8a31c0fd5
Robert Adamyan <robert.adamyan1@gmail.com>
2017-06-20 19:06:36 +0400

    correct problem, solution on small, medium displays

--- components/problem.vue
+++ components/problem.vue
@@ -4,13 +4,12 @@
     <div class="problem-text--wrap">
       <!-- TODO: Problem Icon-->
       <h3 class="problem-item--heading">PROBLEM</h3>
-      <p class="problem-item--main-heading">Traditional supply chain suffers from its productivity and efficiency.</p>
+      <p class="problem-item--main-heading">Traditional supply chain suffers from its productivity and efficiency</p>
       <p class="problem-text">Same transaction can’t be synchronized within multiple companies simultaneously.
-      Trust issues come along when big companies start to cooperate on a large scale.
+      Trust issues come along when big companies start to cooperate on a large scale
       </p>
     </div>
-      </div>
-  
+  </div>
 </template>
 
 <style lang="scss" src="../assets/problem.scss" scoped/>
--- components/solution.vue
+++ components/solution.vue
@@ -2,19 +2,18 @@
 <!-- TODO: provide visuals -->
   <div class="solution-wrap">
     <div class="solution-text--wrap">
-      <h3 class="solution-item--heading">Our Ultimate Solution</h3>
+      <h3 class="solution-item--heading">SOLUTION</h3>
       <p class="solution-item--main-heading">Cryptographically secure private blockchain</p>
       <p class="solution-text">Our blockchain provides a shared, replicated and immutable
         ledger , with assets flowing within it and transactions containing
         metadata about said exchanges
       </p>
       <p class="solution-text"> 
-           Smart contracts system defining rules for transactions and allowing to control entire
-        purchase pipeline.
+        Smart contracts system defining rules for transactions and allowing to control entire
+        purchase pipeline
       </p>
     </div>
-      </div>
-  
+  </div>
 </template>
 
 <style lang="scss" src="../assets/solution.scss" scoped/>
