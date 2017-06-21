368910f960252ab6b476401c1f86f7f12ac8703b
Robert Adamyan <robert.adamyan1@gmail.com>
2017-06-21 20:42:53 +0400

    minor stylistic changes

--- components/problem.vue
+++ components/problem.vue
@@ -3,7 +3,7 @@
     <!-- TODO: X-Shaped Cut-->
     <div class="problem-text--wrap">
       <!-- TODO: Problem Icon-->
-      <h3 class="problem-item--heading">PROBLEM</h3>
+      <h2 class="problem-item--heading"><em>PROBLEM</em></h2>
       <p class="problem-item--main-heading">Traditional supply chain suffers from its productivity and efficiency</p>
       <p class="problem-text">Same transaction can’t be synchronized within multiple companies simultaneously.
       Trust issues come along when big companies start to cooperate on a large scale
