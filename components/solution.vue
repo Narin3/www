af59da4afcdb51533849466c511a806e2374bd4d
Robert Adamyan <robert.adamyan1@gmail.com>
2017-06-21 14:35:30 +0400

    improve solution component, do more compression

--- components/solution.vue
+++ components/solution.vue
@@ -2,7 +2,7 @@
 <!-- TODO: provide visuals -->
   <div class="solution-wrap">
     <div class="solution-text--wrap">
-      <h3 class="solution-item--heading">SOLUTION</h3>
+      <h2 class="solution-item--heading"><em>SOLUTION</em></h2>
       <p class="solution-item--main-heading">Cryptographically secure private blockchain</p>
       <p class="solution-text">Our blockchain provides a shared, replicated and immutable
         ledger , with assets flowing within it and transactions containing
